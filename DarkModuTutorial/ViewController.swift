c0464116aa49d5a1f6cecf10d7724b3e9e0e2130
hamitseyrek <hamitseyrek@gmail.com>
2022-01-19 22:19:25 +0300

    add some code about get user interface mode

--- DarkModuTutorial/ViewController.swift
+++ DarkModuTutorial/ViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        //get interface that user use it
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        // change  changeButton color according to the user interface
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+        } else {
+            changeButton.tintColor = UIColor.blue
+        }
     }
 
 
